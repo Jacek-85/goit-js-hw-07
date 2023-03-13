9eea5da2385ce30e62e9681d0bef4eca4b76f103
Jacek_Blaszczyk <111233513+Jacek-85@users.noreply.github.com>
2023-03-13 22:11:27 +0100

    version 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const imageMarkup = createItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", imageMarkup);
+
+function createItemsMarkup(image) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+        <a class="gallery__link" href="${original.value}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+        </a>
+        </div>`;
+    })
+    .join("");
+}
+
+const clickContainer = (e) => {
+  e.preventDefault();
+
+  if (e.target.classList.contains("gallery")) return;
+  const source = e.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <div class="modal">
+        <img src="${source}"width="800" height="600">
+        
+    </div>
+`);
+
+  instance.show();
+};
+
+galleryContainer.addEventListener("click", clickContainer);
